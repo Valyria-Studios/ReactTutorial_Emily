46f80855b26d4185352f285210b617bc1f7ac148
emilyrainschang <emilyrainschang@gmail.com>
2024-08-27 10:30:12 -0700

    Tutorial Files - Lessons 5/6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-  const title = 'Welcome to the new blog';
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title} </h1>
-        <p>Liked {likes} times</p>
-        <p> [1,2,3,4]</p>
+        <Home />
       </div>
     </div>
   );
