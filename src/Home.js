5f55712f4ab876022c48d5a79d9d9dbec1b03d32
emilyrainschang <emilyrainschang@gmail.com>
2024-08-28 10:56:20 -0700

    Lesson 8-9

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,20 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('emily');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e)
-
-    }
-
-    const handleClickAgain= (name, e) => {
-        console.log('hello, ' + name, e.target);
+    const handleClick = () => {
+        setName('Ms. Chang');
+        setAge(30);
+    
     }
 
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick= {handleClick}>Click me</button>
-            <button onClick = {(e) => handleClickAgain('emily', e)}> Click me again </button>
         </div>
     );
 }
