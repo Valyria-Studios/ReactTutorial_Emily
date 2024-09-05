2fc6d156dbf325e5912dbe51e7dca62657a771e2
emilyrainschang <emilyrainschang@gmail.com>
2024-09-05 16:59:11 -0700

    Test

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,12 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const {data : blogs, isPending, error} = useFetch('http://localhost:3000/blogs');
+  const {data : blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
-  
 
   return (
     <div className="home">
-      { isPending && <div>Loading... Test</div> }
+      { isPending && <div>Loading... TEST 2!!!</div> }
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
