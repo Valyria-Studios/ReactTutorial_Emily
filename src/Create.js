a8cdf33461e9e35a8789e016175e9f6d4c0befa4
emilyrainschang <emilyrainschang@gmail.com>
2024-09-13 13:23:05 -0700

    Lesson 8

--- src/Create.js
+++ src/Create.js
@@ -3,11 +3,16 @@ const Create = ()=> {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const handleSubmit = (e) =>  {
+        e.preventDefault();
+        const blog = {title, body, author}
+        console.log(blog);
+    }
 
     return (
         <div className = "create">
             <h2> Add a New Blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit} >
                 <label>Blog title:</label>
                 <input
                     type = "text" 
@@ -30,9 +35,7 @@ const Create = ()=> {
                     <option value = "yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
-                <p> {title}  </p>
-                <p> {body}  </p>
-                <p> {author}  </p>
+
             </form>
         </div>
     );
